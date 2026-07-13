465fb4addacb91b99e4d0e4dcab7d2cd06d22788
awaiskamil-dev <kamilawais@gmail.com>
2026-07-14 03:21:09 +0500

    Creating logic for rendering board

--- src/Board.jsx
+++ src/Board.jsx
@@ -2,14 +2,22 @@ import { Row } from "./Row";
 import './Board.css'
 
 export function Board({guesses, currentGuess}){
+  const gameOver = guesses.length >= 6;
+  const emptyRows = gameOver? 6 - guesses.length : 6 -guesses.length - 1;
+
   return (
     <div className="board">
-      <Row />
-      <Row />
-      <Row />
-      <Row />
-      <Row />
-      <Row />
+      {guesses.map((guess, index) => {
+        return(
+          <Row key={`guess-${index}`} guess={guess} />
+        );
+      })}
+      {!gameOver && <Row currentGuess={currentGuess} />}
+      {Array.from({length: emptyRows}).map((_, index) => {
+        return(
+          <Row key={`empty-${index}`} />
+        );
+      })}
     </div>
   );
 }
